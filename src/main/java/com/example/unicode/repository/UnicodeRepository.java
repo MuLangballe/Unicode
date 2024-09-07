88456b51f002011b1a458bac9c9a3972b058e098
MuLangballe <mala0007@stud.kea.dk>
2024-09-07 21:11:24 +0200

    UnicodeRepository

---
+++ src/main/java/com/example/unicode/repository/UnicodeRepository.java
@@ -0,0 +1,8 @@
+package com.example.unicode.repository;
+
+import com.example.unicode.model.Unicode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnicodeRepository extends JpaRepository<Unicode, Integer> {
+
+}
