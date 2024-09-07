06ae8d29a20539c8ac6283870a34bfae9c19eacd
MuLangballe <mala0007@stud.kea.dk>
2024-09-07 21:04:04 +0200

    Model pkg with unicode class, connection to MySQL.

---
+++ src/main/java/com/example/unicode/model/Unicode.java
@@ -0,0 +1,46 @@
+package com.example.unicode.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Unicode {
+
+    @Id
+    private int unicode;
+    private char bogstav;
+    private String name;
+    private String description;
+
+    public int getUnicode() {
+        return unicode;
+    }
+
+    public void setUnicode(int unicode) {
+        this.unicode = unicode;
+    }
+
+    public char getBogstav() {
+        return bogstav;
+    }
+
+    public void setBogstav(char bogstav) {
+        this.bogstav = bogstav;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
